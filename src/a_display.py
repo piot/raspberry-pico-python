2e1da1ec180c7358ba8920afed918604e7dbdd8e
Peter Bjorklund <piot@hotmail.com>
2024-02-10 15:45:38 +0100

    fix: formatting

--- src/a_display.py
+++ src/a_display.py
@@ -5,37 +5,42 @@ from machine import Pin, I2C
 import sh1106
 import math
 
-TICKS_PER_SECOND:int = 30
+TICKS_PER_SECOND: int = 30
+
 
 def ticks_to_seconds_left(value: int) -> int:
-    return math.ceil((value + TICKS_PER_SECOND-1)/TICKS_PER_SECOND)
+    return math.ceil((value + TICKS_PER_SECOND - 1) / TICKS_PER_SECOND)
+
 
 def ticks_to_seconds(value: int) -> int:
-    return round(value/TICKS_PER_SECOND)
+    return round(value / TICKS_PER_SECOND)
+
 
 def scan(i2c: I2C):
     devices = i2c.scan()
 
     if len(devices) != 0:
-        print('Number of I2C devices found=',len(devices))
+        print("Number of I2C devices found=", len(devices))
         for device in devices:
-            print("Device Hexadecimal Address= ",hex(device))
+            print("Device Hexadecimal Address= ", hex(device))
     else:
         print("No device found")
-        
+
 
 def initialize_button() -> Button:
     pin: Pin = Pin(13, Pin.IN, Pin.PULL_UP)
     return Button(pin)
 
+
 class Button:
     def __init__(self, pin: Pin):
         self.pin = pin
-    
+
     def is_pressed(self):
         a = self.pin.value()
-        return not a # it is inverted
-        
+        return not a  # it is inverted
+
+
 SCREEN_WIDTH = 128
 SCREEN_HEIGHT = 64
 BALL_SIZE = 8
@@ -43,13 +48,13 @@ WALL_SIZE = 1
 MIDDLE_X = 64
 
 
-class Rating():
+class Rating:
     ARE_YOU_SERIOUS = 0
     TOO_EARLY = 1
-    OK = 2 
+    OK = 2
     PERFECT = 3
     AWESOME = 4
-    
+
     @staticmethod
     def str_value(rating_value):
         rating_map = {
@@ -60,7 +65,8 @@ class Rating():
             Rating.AWESOME: "AWESOME!",
         }
         return rating_map.get(rating_value, str(rating_value))
- 
+
+
 class GamePhase:
     def tick(self, button: Button) -> Bool:
         raise NotImplementedError("must be defined by GamePhases")
@@ -69,7 +75,7 @@ class GamePhase:
 class MainMenu(GamePhase):
     def __init__(self):
         self.was_pressed = True
-        
+
     def tick(self, button: Button):
         is_pressed = button.is_pressed()
         if is_pressed and not self.was_pressed:
@@ -80,12 +86,12 @@ class MainMenu(GamePhase):
 class CountDown(GamePhase):
     def __init__(self, count_down: int):
         self.count_down = count_down
-        
+
     def tick(self, button: Button):
         if self.count_down == 0:
             return True
-        
-        self.count_down -= 1    
+
+        self.count_down -= 1
 
 
 class InGame(GamePhase):
@@ -98,8 +104,8 @@ class InGame(GamePhase):
         self.left_side = WALL_SIZE
         self.lower_side = SCREEN_HEIGHT - BALL_SIZE - WALL_SIZE
         self.upper_side = 0
-        self.x : int = self.left_side
-        self.y : int = self.upper_side
+        self.x: int = self.left_side
+        self.y: int = self.upper_side
         self.speed_increase_every: int = TICKS_PER_SECOND * 4
         self.tick_count: int = 0
         self.ball_bounce_count: int = 0
@@ -110,7 +116,6 @@ class InGame(GamePhase):
         self.rating_count: int = 0
         self.ticks_left: int = TICKS_PER_SECOND * 30
 
-
     def check_bounce_against_walls(self) -> None:
         next_x: int = self.x + self.direction_x * self.speed
         if next_x >= self.right_side:
@@ -120,7 +125,7 @@ class InGame(GamePhase):
             self.direction_x = 1
             next_x = self.left_side
         self.x = next_x
-        
+
         next_y: int = self.y + self.direction_y * self.speed
         if next_y >= self.lower_side:
             self.direction_y = -1
@@ -129,7 +134,7 @@ class InGame(GamePhase):
             self.direction_y = 1
             next_y = self.upper_side
         self.y = next_y
-    
+
     def distance_to_cloest_wall(self) -> int:
         distance_to_left = self.x - self.left_side
         distance_to_right = self.right_side - self.x
@@ -163,21 +168,23 @@ class InGame(GamePhase):
 
     def allowed_to_be_rated(self) -> bool:
         return self.rating_direction == 0
-    
+
     def button_went_down(self):
         if self.allowed_to_be_rated():
             self.rate_button_press()
         else:
             print("not allowed to be rated", self.x, self.rating_direction)
-        
+
     def check_if_allowed_to_be_rated_again(self):
-        if (self.rating_direction > 0 and self.x <= MIDDLE_X) or (self.rating_direction < 0 and self.x > MIDDLE_X):
+        if (self.rating_direction > 0 and self.x <= MIDDLE_X) or (
+            self.rating_direction < 0 and self.x > MIDDLE_X
+        ):
             self.rating_direction = 0
-        
+
     def check_if_speed_should_increase(self):
         if self.tick_count % self.speed_increase_every == 0:
             self.speed += 1
-            
+
     def check_if_button_is_pressed(self):
         button_is_pressed_now = button.is_pressed()
         if button_is_pressed_now and not self.button_was_down_last_tick:
@@ -189,10 +196,10 @@ class InGame(GamePhase):
         if self.ticks_left == 0:
             return True
         self.ticks_left -= 1
-        
+
         self.check_bounce_against_walls()
         self.check_if_allowed_to_be_rated_again()
-        self.check_if_speed_should_increase()            
+        self.check_if_speed_should_increase()
         self.check_if_button_is_pressed()
 
 
@@ -201,7 +208,7 @@ class GameOver(GamePhase):
         self.score = score
         self.was_pressed = True
         self.ticks_left: int = TICKS_PER_SECOND * 2
-    
+
     def tick(self, button: Button):
         if self.ticks_left == 0:
             is_pressed = button.is_pressed()
@@ -211,16 +218,16 @@ class GameOver(GamePhase):
         else:
             self.ticks_left -= 1
 
+
 class Game:
     def __init__(self):
         self.is_over: bool = False
         self.phase = MainMenu()
 
-
     def switch_phase(self) -> GamePhase:
         phase: GamePhase = self.phase
         if isinstance(phase, MainMenu):
-            return CountDown(TICKS_PER_SECOND*3)
+            return CountDown(TICKS_PER_SECOND * 3)
         elif isinstance(phase, CountDown):
             return InGame()
         elif isinstance(phase, InGame):
@@ -229,90 +236,96 @@ class Game:
             return MainMenu()
         else:
             return "Unknown type"
-    
+
     def tick(self, button: Button):
         is_done: Bool = self.phase.tick(button)
         if is_done:
             self.phase = self.switch_phase()
-           
+
 
 class RenderPhase:
     def __init__(self, display: SH1106_I2C):
         self.display = display
-        
+
     def tick(self, button: Button) -> Bool:
         raise NotImplementedError("must be defined by GamePhases")
+
 
 class RenderInGame(RenderPhase):
     def __init__(self, display: SH1106_I2C):
         self.display = display
         self.last_shown_rating_count: int = 0
         self.show_rating_timer: int = 0
-        
+
     def render_rating(self, rating: Rating, score_given: int):
         self.display.text(Rating.str_value(rating), 0, 5, 1)
         self.display.text("bonus:" + str(score_given), 14, 22, 1)
-        
+
     def render_score(self, score: int) -> None:
         self.display.text("Score " + str(score), 30, 48, 1)
-        
+
     def render_ticks_left(self, ticks: int) -> None:
         self.display.text("Time Left " + str(ticks), 10, 38, 1)
-        
+
     def render(self, ingame: InGame):
-        #self.display.text('Hello World!', 128 - ingame.x, 32, 1)
+        # self.display.text('Hello World!', 128 - ingame.x, 32, 1)
         if self.last_shown_rating_count != ingame.rating_count:
             self.show_rating_timer = TICKS_PER_SECOND * 0.5
             self.last_shown_rating_count = ingame.rating_count
-            
+
         if self.show_rating_timer > 0:
             self.show_rating_timer -= 1
             self.render_rating(ingame.last_rating, ingame.last_bonus_given)
         self.render_score(ingame.score)
-        
+
         if ingame.ticks_left < 100:
             self.render_ticks_left(ingame.ticks_left)
-        
+
         self.display.vline(0, 0, 64, 1)
         self.display.vline(127, 0, 64, 1)
         self.display.fill_rect(ingame.x, ingame.y, BALL_SIZE, BALL_SIZE, 1)
-        
+
 
 class RenderMainMenu(RenderPhase):
     def render(self, main: MainMenu):
         self.display.text("Main Menu", 30, 0, 1)
         self.display.text("press button", 12, 52, 1)
-        
+
+
 class RenderGameOver(RenderPhase):
     def render(self, game_over: GameOver) -> None:
         self.display.text("GAME OVER", 30, 20, 1)
         self.display.text("Score " + str(game_over.score), 32, 34, 1)
         if game_over.ticks_left == 0:
             self.display.text("press button", 12, 52, 1)
-            
+
+
 class RenderCountDown(RenderPhase):
     def render(self, count_down: CountDown):
-        self.display.text('Get Ready!', 20, 12, 1)
+        self.display.text("Get Ready!", 20, 12, 1)
         self.display.text(str(ticks_to_seconds(count_down.count_down)), 60, 35, 1)
+
 
 class Render:
     def __init__(self, display: SH1106_I2C):
         self.display = display
         self.phase: RenderPhase = None
-        
+
     def switch_render_phase_if_needed(self, phase: GamePhase):
         if isinstance(phase, MainMenu) and not isinstance(self.phase, RenderMainMenu):
             self.phase = RenderMainMenu(self.display)
-        elif isinstance(phase, CountDown)  and not isinstance(self.phase, RenderCountDown):
+        elif isinstance(phase, CountDown) and not isinstance(
+            self.phase, RenderCountDown
+        ):
             self.phase = RenderCountDown(self.display)
-        elif isinstance(phase, InGame)  and not isinstance(self.phase, RenderInGame):
+        elif isinstance(phase, InGame) and not isinstance(self.phase, RenderInGame):
             self.phase = RenderInGame(self.display)
-        elif isinstance(phase, GameOver)  and not isinstance(self.phase, RenderGameOver):
+        elif isinstance(phase, GameOver) and not isinstance(self.phase, RenderGameOver):
             self.phase = RenderGameOver(self.display)
-        
+
     def render(self, phase: GamePhase):
         self.display.fill(0)
-        self.switch_render_phase_if_needed(phase)            
+        self.switch_render_phase_if_needed(phase)
         self.phase.render(phase)
         self.display.show()
 
@@ -320,13 +333,15 @@ class Render:
 def initialize_i2c() -> I2C:
     sdaPIN = Pin(2)
     sclPIN = Pin(3)
-    i2c = I2C(1,sda=sdaPIN, scl=sclPIN, freq=400000)
+    i2c = I2C(1, sda=sdaPIN, scl=sclPIN, freq=400000)
     return i2c
 
+
 def initialize_display() -> SH1106_I2C:
-    display : SH1106_I2C = sh1106.SH1106_I2C(128,64,i2c)
+    display: SH1106_I2C = sh1106.SH1106_I2C(128, 64, i2c)
     display.sleep(False)
     return display
+
 
 i2c: I2C = initialize_i2c()
 
